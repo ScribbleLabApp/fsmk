eddc3a70525ebabf2a5876b5f32fc95d0802b414
N3v1 <n3v1010@gmail.com>
2024-11-15 20:52:20 +0100

    (chore): Update cyfn copyright and add fsec

--- fsmk/src/cyfn.c
+++ fsmk/src/cyfn.c
@@ -3,6 +3,7 @@
 //  ScribbleLab FSMK Core
 //
 //  Copyright (c) 2024, ScribbleLabApp LLC
+//  Copyright (c) 2012 - 2024, kokke tiny-AES-c - All rights reserved.
 //
 //  Redistribution and use in source and binary forms, with or without
 //  modification, are permitted provided that the following conditions are met:
